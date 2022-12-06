87e9a6aeb3edae44fc7b889ac365386726ad6f12
Evan Chng Xian Lin <xianlinchng@gmail.com>
2022-12-06 11:59:09 +0800

    Add SolidJS frontend

---
+++ apps/app/src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post()
+  async findToken(@Body() tokenAddress: string): Promise<string[]> {
+    return this.appService.findToken(tokenAddress);
+  }
+}
